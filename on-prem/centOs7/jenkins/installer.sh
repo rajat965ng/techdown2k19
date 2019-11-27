cd94cad8e6d75e1be2d3e1931a3bba09c20f4bbb
Rajat Nigam <rajnigam@publicisgroupe.net>
2019-11-27 14:43:14 +0530

    update docker installer in jenkins

--- on-prem/centOs7/jenkins/installer.sh
+++ on-prem/centOs7/jenkins/installer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo yum install java-1.8.0-openjdk-devel curl -y
+sudo yum install docker -y && sudo systemctl enable docker && sudo systemctl start docker
 
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 
@@ -10,3 +11,7 @@ sudo yum install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 sudo systemctl enable jenkins
+
+sudo groupadd docker
+sudo chown jenkins:docker /var/run/docker.sock
+sudo usermod -aG docker jenkins
