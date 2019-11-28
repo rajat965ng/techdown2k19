928026a2d5ed035da5ea52722ac3707b3dac6e0c
Rajat Nigam <rajnigam@publicisgroupe.net>
2019-11-28 07:50:54 +0530

    add nginx installer

---
+++ on-prem/centOs7/nginx/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo yum install epel-release nginx -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
+sudo systemctl reload nginx
+
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+
