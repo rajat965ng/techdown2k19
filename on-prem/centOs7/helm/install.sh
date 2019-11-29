5d2112730298d3ea59d66f62e78e16a90a3ea96f
Rajat Nigam <rajnigam@publicisgroupe.net>
2019-11-29 10:41:52 +0530

    adding helm

---
+++ on-prem/centOs7/helm/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo yum install curl -y;
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
