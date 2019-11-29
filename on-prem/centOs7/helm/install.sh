c0b176b872b4229fd84eba341b8dc7b7c78e7cef
Rajat Nigam <rajnigam@publicisgroupe.net>
2019-11-29 16:56:51 +0530

    update with es deployment yaml

--- on-prem/centOs7/helm/install.sh
+++ on-prem/centOs7/helm/install.sh
@@ -5,3 +5,13 @@ sudo yum install curl -y;
 sudo wget https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 --output-document=get_helm.sh
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
+
+#helm repo add elasticsearch https://hub.helm.sh/charts/elastic/elasticsearch
+#
+#helm search repo elasticsearch https://hub.helm.sh/charts/elastic/elasticsearch
+#
+#
+#helm install --name elasticsearch https://hub.helm.sh/charts/elastic/elasticsearch
+#helm install filebeat https://hub.helm.sh/charts/elastic/filebeat
+#helm install kibana https://hub.helm.sh/charts/elastic/kibana
+#helm install metricbeat https://hub.helm.sh/charts/elastic/metricbeat
