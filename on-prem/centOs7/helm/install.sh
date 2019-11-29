b92979d9692cb03346adeaae3fd6344122118611
Rajat Nigam <rajnigam@publicisgroupe.net>
2019-11-29 10:44:39 +0530

    give sudo access in helm setup

--- on-prem/centOs7/helm/install.sh
+++ on-prem/centOs7/helm/install.sh
@@ -2,6 +2,6 @@
 
 sudo yum install curl -y;
 
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+sudo curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 sudo chmod 700 get_helm.sh
 sudo ./get_helm.sh
